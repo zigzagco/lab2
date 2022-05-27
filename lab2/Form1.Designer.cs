a94339c20db9332bc0e21c5e38d4ee949ca5862c
gleb <gleb@DESKTOP-DAOTA9Q>
2022-05-28 01:13:45 +0300

    Добавьте файлы проекта.

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "получить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(773, 375);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+        String str;
+        int i=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList lastname = new ArrayList();
+            ArrayList firstname = new ArrayList();
+            ArrayList oldname = new ArrayList();
+            ArrayList group = new ArrayList();
+            ArrayList index = new ArrayList();
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string[] lines = File.ReadAllLines(filename);
+
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "Фамилия";
+            dataGridView1.Columns[1].Name = "Имя";
+            dataGridView1.Columns[2].Name = "Отчествво";
+            dataGridView1.Columns[3].Name = "Группа";
+            dataGridView1.Columns[4].Name = "средний балл";
+            dataGridView1.RowHeadersVisible = false;
+
+            foreach (string s in lines)
+            {
+                Match match = Regex.Match(s, "ФИО:(.*?)ЛН:(.*?),средний бал:(.*?),");
+                Match fio = Regex.Match(match.Groups[1].ToString(), "(.*?) (.*?) (.*?),");
+                if (fio.Groups[1].Value.ToString()!=" ")
+                {
+                    lastname.Add(fio.Groups[1].Value);
+                }
+                else
+                {
+                    lastname.Add("");
+                }
+                firstname.Add(fio.Groups[2].Value);
+                oldname.Add(fio.Groups[3].Value);
+                group.Add(match.Groups[2].Value);
+                index.Add(match.Groups[3].Value);
+                
+            }
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine("    "+oldname[i]);
+                dataGridView1.Rows.Add(lastname[i], firstname[i], oldname[i], group[i], index[i]);
+            }
+
+        }
+
+      
+    }
+    
+}
